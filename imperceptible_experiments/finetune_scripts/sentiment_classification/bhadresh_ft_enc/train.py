c6b7f8698d6c942ba466b8b9a1e55a44f08f6efa
Victor <victorlohwk@gmail.com>
2025-05-11 02:47:38 +0100

    fix dumb bug

--- imperceptible_experiments/finetune_scripts/sentiment_classification/bhadresh_ft_enc/train.py
+++ imperceptible_experiments/finetune_scripts/sentiment_classification/bhadresh_ft_enc/train.py
@@ -169,7 +169,7 @@ def main():
         print(f"Validation Accuracy: {acc:.4f}")
         if acc > best_acc:
             best_acc = acc
-            torch.save(model.state_dict(), os.path.join(MODEL_DIR, "model.pt"))
+            model.save_pretrained(MODEL_DIR)
             tokenizer.save_pretrained(MODEL_DIR)
             print("Best model saved")
 
